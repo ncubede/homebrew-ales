4de324cef2a0241de324d3901c00be062d167aa3
Stephan Trebels <stephan.trebels@arris.com>
2019-02-21 13:39:06 +0100

    rtc:000000 - pycurl #1

---
+++ Formula/pycurl.rb
@@ -0,0 +1,20 @@
+require 'formula'
+
+class Pycurl < Formula
+  homepage 'http://pycurl.io/'
+  url 'https://dl.bintray.com/pycurl/pycurl/pycurl-7.43.0.2.tar.gz'
+  sha256 '0f0cdfc7a92d4f2a5c44226162434e34f7d6967d3af416a6f1448649c09a25a4'
+
+  depends_on 'coreutils'
+  depends_on 'libxml2' => 'with-python'
+  depends_on 'python@2'
+
+  def install
+    pypref = `python-config --prefix`.chomp
+    pybin = "#{pypref}/bin/python"
+
+    system pybin, 'setup.py', 'build'
+    system pybin, 'setup.py', 'install', "--prefix=#{prefix}"
+  end
+
+end
