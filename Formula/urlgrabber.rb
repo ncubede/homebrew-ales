e69a120a536893700f246e67b89d275b47570648
Stephan Trebels <stephan.trebels@arris.com>
2019-02-21 17:28:37 +0100

    rtc:000000 - testing

--- Formula/urlgrabber.rb
+++ Formula/urlgrabber.rb
@@ -6,7 +6,7 @@ class Urlgrabber < Formula
   url 'http://urlgrabber.baseurl.org/download/urlgrabber-3.10.2.tar.gz'
   sha256 '53691185e3d462bb0fa8db853a205ee79cdd4089687cddd22cabb8b3d4280142'
 
-  depends_on ncubede/ales/pycurl
+  depends_on 'ncubede/ales/pycurl'
 
   def install
     python_prefix = `python-config --prefix`.chomp
