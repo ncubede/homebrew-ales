315db8b95ee6b9a260d0802bd3b513f171eaf9b2
Stephan Trebels <stephan.trebels@arris.com>
2019-02-21 17:19:43 +0100

    rtc:000000 - use namespacing instead of versions

--- Formula/yum.rb
+++ Formula/yum.rb
@@ -7,7 +7,7 @@ class Yum < Formula
   sha256 '0178f97820ced9bfbcc269e6fc3ea35e29e35e2d263d24c7bff8660ee62d37ca'
 
   depends_on 'ncubede/ales/rpm-python'
-  depends_on 'ncubede/ales/yum-metadata-parser@1.1.4'
+  depends_on 'ncubede/ales/yum-metadata-parser'
   depends_on 'ncubede/ales/urlgrabber'
 
   def install
