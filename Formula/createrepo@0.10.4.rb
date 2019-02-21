f4468b64425fc0da7831c928d979afd5f954e2bf
Stephan Trebels <stephan.trebels@arris.com>
2019-02-21 11:34:25 +0100

    rtc:000000 - add createrepo@0.10.4 and yum-metadata-parser@1.1.4

---
+++ Formula/createrepo@0.10.4.rb
@@ -0,0 +1,38 @@
+require 'formula'
+
+# Main class describing formula
+class CreateRepoAT0104 < Formula
+  homepage 'http://createrepo.baseurl.org//'
+  url 'http://createrepo.baseurl.org/download/createrepo-0.10.4.tar.gz'
+  sha256 'f850504a3ec7e556aaaa626d487e8b0def84ffea666fd30774cbbd575c128261'
+
+  depends_on 'ncubede/ales/yum-metadata-parser@1.1.4'
+  depends_on 'ncubede/ales/rpm-python'
+  depends_on 'coreutils'
+  depends_on 'libxml2' => 'with-python'
+
+  def install
+    inreplace(
+      ['bin/createrepo', 'bin/modifyrepo'], '/usr/share/createrepo', "#{HOMEBREW_PREFIX}/share/createrepo")
+
+    inreplace(
+      ['dmd.py', 'dumpMetadata.py', 'genpkgmetadata.py', 'readMetadata.py'], '/usr/bin/python', '/usr/bin/env python')
+
+    system(
+      'make', "prefix=#{prefix}", 'INSTALL=ginstall -p --verbose', 'install')
+  end
+
+  def caveats
+    keg_site_packages = "#{Formula['libxml2'].opt_prefix}/lib/python2.7/site-packages"
+    user_site_packages = `python -m site --user-site`.strip
+    msg = <<-EOS
+Please make sure python can find libxml2 module.
+There's couple of ways to achieve that, libxml2 module should give you
+relevant hint when installing. One of the ways that might work for you:
+  mkdir -p #{user_site_packages}
+  echo 'import site; site.addsitedir("/usr/local/lib/python2.7/site-packages")' >> #{user_site_packages}/homebrew.pth
+  echo "#{keg_site_packages}" >> /usr/local/lib/python2.7/site-packages/libxml2.pth
+EOS
+    msg
+  end
+end
---
+++ Formula/yum-metadata-parser@1.1.4.rb
@@ -0,0 +1,22 @@
+require 'formula'
+
+# Main class describing formula
+class YumMetadataParserAT114 < Formula
+  homepage 'http://yum.baseurl.org/'
+  url 'http://yum.baseurl.org/download/yum-metadata-parser/yum-metadata-parser-1.1.4.tar.gz'
+  sha256 'f1560a284541feb720c3ae35b6b31f80c2be5cb9ca86b3a1ee9459846f821ada'
+
+  depends_on 'pkg-config' => :build
+  depends_on 'glib'
+  depends_on 'sqlite3'
+  depends_on "python@2"
+
+  def install
+    # Fix for removed python. stuff... Argh!
+    pypref = `python-config --prefix`.chomp
+    pybin = "#{pypref}/bin/python"
+
+    system pybin, 'setup.py', 'build'
+    system pybin, 'setup.py', 'install', "--prefix=#{prefix}"
+  end
+end
